f5ed959479a26452b6ec562b41be07239caf346e
Crolwick <UIGiren@gmail.com>
2023-09-20 18:56:16 -0700

    architecture finished

--- backend/allData/resizing.py
+++ backend/allData/resizing.py
@@ -3,19 +3,19 @@ import numpy as np
 
 def crop(imgArray):
 
-    croppedArray = np.array([])
+    croppedArray = []
 
     for image in imgArray:
         greyImg = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
         faceCascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
-        face = faceCascade.detectMultiScale(greyImg, 1.1, 4)
+        face = faceCascade.detectMultiScale(greyImg, 1.1, 2)
 
         for (x, y, w, h) in face:
             cv.rectangle(greyImg, (x, y), (x+w, y+h), (0, 0, 255), 2)
             face = greyImg[y:y + h, x:x + w]
-            croppedArray = np.append(croppedArray, face)
-            #cv.imshow("face",face)
+            croppedArray.append(face)
+            #cv.imshow("face", face)
             #cv.waitKey(0)
 
     return croppedArray
--- backend/network/architecture.py
+++ backend/network/architecture.py
@@ -2,44 +2,64 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import nn # nn = NeuralNetwork
 from keras import layers, models, Input
-from keras.layers import Conv2D, Conv3D, ReLU, MaxPool2D, Dense, Flatten, Activation, Dropout, BatchNormalization
+from keras.layers import Conv2D, Conv3D, ReLU, MaxPool2D, Dense, Flatten, Activation, Dropout, BatchNormalization, LSTM, TimeDistributed, Bidirectional
 
 
 # image = single image
 def neuralNetwork():
-
+    '''
     model = models.Sequential([
-        Input(shape= (8, 8, 3)),
-        Conv2D(filters=32, kernel_size=(3, 3), strides=(2, 2)),
-        BatchNormalization(),
-        Activation('relu'),
-        MaxPool2D(pool_size=(2, 2), strides=(1, 1)),
-        Dropout(0.3),
+        Input(shape= (64, 64, 1)),
 
-        Conv2D(filters=64, kernel_size=(3, 3), strides=(2, 2)),
-        BatchNormalization(),
-        Activation('relu'),
+        TimeDistributed(Conv2D(filters=32, kernel_size=(3, 3), padding='same', strides=(2, 2), activation='relu')),
+        TimeDistributed(BatchNormalization()),
 
-        Conv2D(filters=32, kernel_size=(3, 3), strides=(2, 2)),
-        BatchNormalization(),
-        Activation('relu'),
-        MaxPool2D(pool_size=(2, 2), strides=(1, 1)),
-        Dropout(0.3),
+        TimeDistributed(MaxPool2D(pool_size=(2, 2))),
+        TimeDistributed(Dropout(0.25)),
 
-        Flatten(),
-        Dense(64, activation='relu'),
-        BatchNormalization(),
-        Dense(128, activation='relu'),
-        BatchNormalization(),
-        Dropout(0.5),
-        Dense(64, activation='relu'),
-        BatchNormalization(),
-        Dense(32, activation='relu'),
-        BatchNormalization(),
+        TimeDistributed(Conv2D(filters=64, kernel_size=(3, 3), padding='same', strides=(2, 2), activation='relu')),
+        TimeDistributed(BatchNormalization()),
+
+        TimeDistributed(MaxPool2D(pool_size=(2, 2))),
+        TimeDistributed(Dropout(0.25)),
+
+        TimeDistributed(Conv2D(filters=20, kernel_size=(3, 3), padding='same', strides=(2, 2), activation='relu')),
+        TimeDistributed(BatchNormalization()),
+
+        TimeDistributed(Flatten()),
+
+        Bidirectional(LSTM(32, kernel_initializer='Orthogonal', return_sequences=True)),
+        Dropout(0.25),
 
         Dense(20, activation='softmax')
+    ])
+    '''
+    model = models.Sequential([
+        Input(shape= (None, 64, 64, 1)),
 
-    ]) # Using Convolutional NN Base
+        TimeDistributed(Conv2D(filters=32, kernel_size=(3, 3), padding='same', strides=(2, 2), activation='relu')),
+        BatchNormalization(),
+
+        TimeDistributed(MaxPool2D(pool_size=(2, 2))),
+        Dropout(0.25),
+
+        TimeDistributed(Conv2D(filters=64, kernel_size=(3, 3), padding='same', strides=(2, 2), activation='relu')),
+        BatchNormalization(),
+
+        TimeDistributed(MaxPool2D(pool_size=(2, 2))),
+        Dropout(0.25),
+
+        TimeDistributed(Conv2D(filters=20, kernel_size=(3, 3), padding='same', strides=(2, 2), activation='relu')),
+        BatchNormalization(),
+
+        TimeDistributed(Flatten()),
+
+        Bidirectional(LSTM(32, kernel_initializer='Orthogonal', return_sequences=True)),
+        Dropout(0.25),
+
+        Dense(20, activation='softmax')
+    ])
+    # Using Convolutional NN Base
 
     # Sample (WIP Using Keras)
     # layers.Conv2d
@@ -53,6 +73,6 @@ def neuralNetwork():
     model.summary()
     return model
 
-model = neuralNetwork()
 
-model.compile()
+
+
