8d4f56669de33ca56cbd4bd5d7f0c171173e43a1
TempuraSoup <travis041112@gmail.com>
2023-09-30 23:16:36 -0700

    frontend => arrows

--- frontend/src/frontPage.jsx
+++ frontend/src/frontPage.jsx
@@ -1,17 +1,33 @@
 import image from "./color_005.jpg"
 import crop from "./singleImage.jpg"
 import colorCrop from "./singleCropImageColor.jpg"
+import rightArrow from "./arrow_right.webp"
+import arch from "./neural_network_architecture.jpeg"
 
 function Images(){
 
     return(
         <>
         
-        <div className="items-center justify-center">
-            <img src={image} className="inline-flex mr-20"/>
-            <img src={colorCrop} className="inline-flex mr-20"/>
-            <img src={crop} className="inline-flex"/>
+        <div className="inline-flex justify-center relative">
+            <img src={image} className="mr-20"/>
+
+            <div className="absolute font-bold">
+                <p>Original Image</p>
+
+            </div>
         
+        </div>
+        
+        <img src={rightArrow} className="inline-flex l-20 w-20 mr-20"/>
+
+        <div className="items-center justify-center relative">
+            <img src={colorCrop} className="inline-flex mr-20"/>
+            <img src={rightArrow} className="inline-flex l-20 w-20 mr-20"/>
+            <img src={crop} className="inline-flex mr-20"/>
+            <img src={rightArrow} className="inline-flex l-20 w-20 mr-20"/>
+            <img src={arch} className="inline-flex l-1/3 w-1/3 mr-20"/>
+
         </div>
         
         </>
